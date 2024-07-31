60145cd0ccafab08f15e65a1343dd6d74d254f80
Yevhen Kariev <mediams@mail.ru>
2024-07-31 16:41:30 +0200

    31-07-2024

---
+++ FrontEnd/lesson_11/script/script.js
@@ -0,0 +1,118 @@
+/* console.log('Hello Welt! Test')
+console.log(5+5)
+document.write(1+2) */
+
+
+// alert('Hello!')
+
+
+/* let a=12
+let b=23
+console.log(a+b)
+a=2
+const c=2
+console.log('Die Summe von', a, 'plus', c, 'ist gleich', a+b,'.')
+const w = 'Hi'
+console.log(w)
+console.log(`${a} mal ${c} ist gleich ${a * c}.`);
+const user_Name = 'wo' */
+
+
+// ----------------------------------
+// const userName = prompt('Geben Sie Ihren Namen ein.')
+// const userNachName = prompt('Gib deinen Nachnamen ein.')
+// console.log(`Name: ${userName} Nachname: ${userNachName}`)
+
+/* const num1 = Number (prompt())
+const num2 = Number (prompt()) */
+
+/* const num1 = +prompt()
+const num2 = +prompt() */
+
+// console.log(num1*num2, `${num1}*${num2}`)
+
+
+/* console.log(typeof(w))
+console.log(typeof(a)) */
+
+// console.log(num1 + num2)
+
+/* const str1='Hello '
+const str2='Welt!'
+console.log(str1+str2) */
+
+
+// -------------------------------------------
+/* const Name = prompt('Geben Sie Ihren Namen')
+const age = prompt('Gib dein alt') */
+
+// console.log('Hallo ' + Name + ',  du bist  ' + age + ' Jahre alt ')
+/* console.log(`Hallo ${Name}, du bist ${age} Jahre alt.`)   */
+// *******************************************************
+
+
+
+// ---------------------------------------------------------Задача: есть переменные r, g, b с числовыми // значениями. Вывести в консоль строку “rgb(12, 34, 14)” 
+// используя конкатенацию и интерполяцию
+// Задача: есть переменные r, g, b с числовыми 
+// значениями. Вывести в консоль строку “rgb(12, 34, 14)” 
+// используя конкатенацию '+r+'  и интерполяцию ${r}
+
+/* const r = 12
+const g = 34
+const b = 14
+console.log('rgb(' + r + ', ' + g + ', ' + b + ')');
+console.log('rgb('+r+', '+g+', '+b+')')
+console.log(`rgb(${r}, ${g}, ${b})`) */
+
+// ****************************************************
+
+
+// ---------------------------------------------Написать программу, которая запрашивает у пользователя его возраст (в годах) и выводит, сколько ему лет.
+/* const age = prompt('Gib dein alt')
+const dateNow=new Date().getFullYear();
+console.log(`Du bist ${age} Jahre alt.`)
+console.log(`Sie sind ${dateNow-age} geboren.`) */
+// ***********************************************
+
+
+// ----------------------------------Задача: Написать программу, которая считывает через prompt число и выводит в консоль ее квадрат
+/* const num = prompt('Gib nummer')
+console.log(`Das Quadrat von ${num} ist gleich ${num**2}`) */
+// *************************************************************
+
+
+// ---------------------------------------------KOren 
+/* console.log(Math.sqrt(1000))
+console.log(17%3) */
+// ************************************************
+
+
+// -----------------------------Написать программу, которая считывает два числа (объявляем две переменные и записываем туда результат работы двух вызовов prompt) и выводит их сумму. Не забудьте преобразовать полученные значения в число.
+/* const num1 = +(prompt('Gib nummer 1'))
+const num2 = Number (prompt('Gib nummer 2'))
+console.log(`Die Summe ${num1} + ${num2} ist gleich ${num1+num2}.`) */
+// ********************************************************
+
+
+
+// ----------------------------------------------SRAVNENIE
+/* console.log(4>5) //false
+console.log(4<5) //true
+console.log(4<=5) //true
+console.log(4>=5) //false
+console.log(4==5) //false
+console.log(5===5) //true
+console.log(5=='5') // ne strogoe sravnenie
+console.log(5===`5`) // strogoe sravnenie
+console.log(4!==5) //true
+console.log(5!==5) //false */
+
+
+/* console.log('a'==='A') // false
+console.log('A'==='A') // true
+console.log('a'=='A') // false
+console.log('numb'>'numa') // true
+console.log('a'<'z') //true */
+// ********************************************
+
