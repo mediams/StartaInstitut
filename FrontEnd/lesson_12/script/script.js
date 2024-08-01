3c00626f0a24ddd552d2e0f238c5b45d3b608c9d
Yevhen Kariev <mediams@mail.ru>
2024-08-01 13:10:43 +0200

    Работа на компьютере

--- FrontEnd/lesson_12/script/script.js
+++ FrontEnd/lesson_12/script/script.js
@@ -28,8 +28,8 @@ else {
     console.log(`Число ${num2} больше чем ${num1}`);
 } */
 
-
-const num =prompt()
+// -------------------------------------
+/* const num =prompt()
 console.log(`Вы ввели: ${num}`);
 if (num%2===0 && num>0){
     console.log(`Это чётное число И оно положительное`);
@@ -43,6 +43,99 @@ if (num%2===0 && num>0){
     console.log(`Это чётное число и оно отрицательное`);
 }else{
     console.log(`Рекомендую ввести число!`);
+} */
+// ********************************************
+
+
+
+
+
+// ---------------------------------------------------------
+/* const arr=[12, -8, "lol", true]
+console.log(typeof arr);
+console.log(arr);
+console.log(arr[3]);
+arr[2]=9
+console.log(arr);
+arr[19]=161616
+console.log(arr);
+
+arr.push("new push") //Add Wert am Ende
+console.log(arr);
+
+arr.pop()   //Löschen Wert am Ende
+console.log(arr);
+arr.pop()
+console.log(arr);
+arr.pop()
+console.log(arr);
+arr.pop()
+console.log(arr);
+arr.pop()
+console.log(arr);
+arr.push("new push") //Add Wert am Ende
+console.log(arr);
+
+console.log(`Add Wert am Anfang`);
+arr.unshift(777) //Add Wert am Anfang
+console.log(arr);
+console.log(`Löschen Wert am Anfang`);
+arr.shift() //Löschen Wert am Anfang
+console.log(arr); */
+// ***************************************************************
+
+
+
+
+// ----------------------------------------------
+// создать программу, которая считывает три числа через prompt и добавляет их в массив.
+/* const arr1=[]
+const num1 =+prompt()
+const num2 =+prompt()
+const num3 =+prompt()
+
+arr1.push(num1, num2, num3) //Add Wert am Ende
+console.log(arr1); */
+// *************************************************
+
+/* Задание: Написать программу, в которой объявлен массив с 5 числовыми элементами. Программа должна заполнить новый пустой массив квадратами чисел из первого массива.
+Пример: 
+Исходный массив [1, 4, 2, 5, 3]
+Итоговый массив [1, 16, 4, 125 9] */
+
+
+/* const arr=[1, 4, 2, 5, 3]
+const arr2=[]
+console.log(arr);
+arr[0]=arr[0]**2
+arr[1]=arr[1]**2
+arr[2]=arr[2]**2
+arr[3]=arr[3]**2
+arr[4]=arr[4]**2
+
+console.log(arr);
+
+arr2.push(arr[0]**2, arr[1]**2, arr[2]**2, arr[3]**2, arr[4]**2)
+console.log(arr2); */
+
+// ----------------------------------------
+const arr=[1, 4, 2, 5, 3]
+console.log(`Schleifen`);
+
+// arr=[11, 40, 2, 5, 3]
+arr3=[11, 40, 2, 5, 3]
+console.log(`Числа в массиве ${arr3}`);
+
+for(let i=0; i<arr3.length; i=i+2){
+    arr3[i]=arr3[i]**2
 }
+console.log(`Числа в массиве после возведения в корень ${arr3}`);
+console.log(arr3);
+// ************************************************
 
-
+// ------------------------------------------
+/* let arr=[1, 4, 2, 5, 3]
+console.log(arr);
+arr=[11, 40, 2, 5, 3]
+console.log(arr); */
+// ***********************************************
