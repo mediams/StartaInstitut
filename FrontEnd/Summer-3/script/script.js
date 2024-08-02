e4d2177d0c2ce1221a167f0d71b96e9dacf2e077
Yevhen Kariev <mediams@mail.ru>
2024-08-02 17:04:39 +0200

    02-08-24

---
+++ FrontEnd/Summer-3/script/script.js
@@ -0,0 +1,80 @@
+let var_1 = 20;
+let var_2 = 40;
+
+//console.log();
+// alert('Stop!');
+//document.write();
+
+// let var_3 = prompt();
+
+//const pi = 3.14;
+//pi = 20                // ERROR */
+//pi += pi;
+
+
+/* let name1 = `Ya`;
+let name2 = `Net`; */
+
+
+// console.log(`Dein Name ist `+name1 +` ` +name2);
+/* console.log(`Dein Name ist ${name1} ${name2}`); */
+
+// Задание: Написать программу, которая запрашивает у пользователя Имя, Фамилию, Возраст и выводит в консоль строку 
+// “Hello, my name is UserName UserSurname. I’m UserAge.”. 
+// Для вывода использовать два варианта: конкатенация и интерполяция.
+
+// alert('Gehen wir weiter?');
+// let name_1 = /* prompt */(`Имя`);
+// let name_2 = /* prompt */ (`Фамилию`);
+// let age = /* +prompt */(`Возраст`);
+// let age_test = /* +prompt */(`Возраст`)
+
+// console.log(`Hello, my name is ${name_1} ${name_2}. I’m ${age}.`);
+// console.log('Hello, my name is '+name_1+' '+name_2+'. I’m '+age+'.');
+
+
+// Задание : Написать программу, которая считывает два числа (объявляем две переменные (a b) и записываем туда результат работы двух вызовов prompt) и выводит их разницу, сумму и произведение.
+// Вывод должен быть по шаблону: (“Разность чисел a и b равна sub, сумма чисел a и b равна sum, произведение чисел a и b равно mult”)
+
+// let a = +prompt(`a`);
+// let b = +prompt (`b`);
+// console.log(`Разность чисел ${a} и ${b} равна ${a-b}, сумма чисел ${a} и ${b} равна ${a+b}, произведение чисел ${a} и ${b} равно ${a*b}`);
+
+// Задачи на условные операторы и циклы: 
+// Объявить три числовых переменных и вывести наибольшее из них.
+// Вывести все числа от 1 до 10, которые без остатка делятся на 3.
+// Вывести все четные числа от 1 до 100.
+
+/* let num_1=77
+let num_2=40
+let num_3=56 */
+
+let num_1 = +prompt(`a`);
+let num_2 = +prompt (`b`);
+let num_3 = +prompt(`c`);
+
+// for (let i=0; i<=3;i++){
+    // if(num_1<num_2){
+    //     if(num_2<num_3){
+    //         console.log(`Вы ввели числа ${num_1} и ${num_2} и ${num_3} число  ${num_3} наибольшее`);            
+    //     }else console.log(`Вы ввели числа ${num_1} и ${num_2} и ${num_3} число  ${num_2} наибольшее`);
+    // } else console.log(`Вы ввели числа ${num_1} и ${num_2} и ${num_3} число  ${num_1} наибольшее`);
+// }
+
+// for(let i=1; i<=10; i++){
+//     if (i%3===0){
+//         console.log(i);
+//     } 
+//     // console.log(i);   
+// }
+
+// let arr_1 = [];
+// for(let i=1; i<=100; i++){
+//     if (i%2===0){
+//         arr_1.push(i);
+//     }  
+// }
+// console.log(arr_1);  
+
+
+
